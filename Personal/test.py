b9c652d396bbc4a52e749ff379b9afaadc05fd65
Daniil Kazachanskiy <85735195+Slaesh-26@users.noreply.github.com>
2024-02-14 22:02:04 +0300

    Delete directory

--- Personal/test.py
+++
@@ -1,8 +0,0 @@
-def get_values_list(filename : str):
-    file = open(filename, 'r', encoding = "utf-8")
-    values_raw = file.read().split(',')
-    values_cast = [float(x) for x in values_raw]
-    file.close()
-    return values_cast
-
-print(get_values_list("input.csv"))
